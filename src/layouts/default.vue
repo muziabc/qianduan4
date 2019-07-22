ee68cf62f0c5a94a755f42ae70dd1d6d2c03cba9
test <test@qq.com>
2019-07-22 18:52:14 +0800

    source code push

---
+++ src/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <header-nav-bar></header-nav-bar>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import HeaderNavBar from '@/components/header-nav-bar'
+
+export default {
+  components: {
+    'header-nav-bar': HeaderNavBar
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style lang="stylus">
+.wrapper
+  text-align: center;
+</style>
